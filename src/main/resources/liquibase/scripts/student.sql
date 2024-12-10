f5b8337d486524c9ce2be5edad1d4e97347be0e6
Student Free <l3ra131007@gmail.com>
2024-12-10 15:52:43 +0300

    .

--- src/main/resources/liquibase/scripts/student.sql
+++ src/main/resources/liquibase/scripts/student.sql
@@ -1,8 +1,9 @@
 -- liquibase formatted sql
 
 --changeset ddavydov:1
+CREATE INDEX users_name_index ON student (name);
 
-CREATE TABLE users(
-    id SERIAL,
-    email TEXT
-)
+--changeset ddavydov:2
+CREATE INDEX faculty_name_index ON faculty (name);
+CREATE INDEX faculty_color_index ON faculty (color);
+    
